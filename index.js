bbb4c70671a047042d5e6feb761be1c73f870416
mikimaus1311 <134337198+mikimaus1311@users.noreply.github.com>
2023-11-07 02:07:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ setInterval(() => {
 // U CAN ONLY EDIT THIS SECTION!!
 function createBot () {
 const bot = mineflayer.createBot({
-  host: 'pollock.aternos.host', 
+  host: 'tarpon.aternos.host', 
   version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
   username: 'Gapolt', 
   port: 61705,
